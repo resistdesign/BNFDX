63cedc28eaffcbb2a4555ec9c08e00d0d0aaa027
Ryan Graff <ryantgraff@resistdesign.com>
2021-02-16 23:15:55 -0500

    feat: (index) Add the basis for parsePathD

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,53 @@
 import React, { FC, HTMLAttributes } from 'react';
 
-export namespace R3DM {
-  export type PathProps = HTMLAttributes<SVGPathElement> & {};
+export type PathCommand = {
+  command: string;
+  coords: number[];
+  length: number;
+};
 
-  export const Path: FC<PathProps> = ({ ...props }) => {
-    return <path {...props} />;
-  };
+export const parsePathD = (d = ''): PathCommand[] =>
+  d
+    .replace(/[a-z]/gim, (char) => ` :${char} `)
+    .replace(/[+-]/gim, (char) => ` ${char}`)
+    .replace(/,/gim, ' ')
+    .split(' ')
+    .filter((chars) => chars !== '')
+    .join(' ')
+    .split(':')
+    .filter((chars) => chars !== '')
+    .reduce((acc, data): PathCommand[] => {
+      const command = data.split('').filter((char) => /[a-z]/gim.test(char))[0];
+      const coords = data
+        .split(' ')
+        .filter((chars, i) => !!chars && i > 0)
+        .map((v) => parseFloat(v));
+      const { length } = coords;
 
-  export type GroupProps = HTMLAttributes<SVGGElement> & {};
+      return [
+        ...acc,
+        {
+          command,
+          coords,
+          length,
+        },
+      ];
+    }, [] as PathCommand[]);
 
-  export const Group: FC<GroupProps> = ({ children, ...props }) => {
-    return <g {...props}>{children}</g>;
-  };
+export type PathProps = HTMLAttributes<SVGPathElement> & {};
 
-  export type ViewProps = HTMLAttributes<SVGElement> & {};
+export const Path: FC<PathProps> = ({ ...props }) => {
+  return <path {...props} />;
+};
 
-  export const View: FC<ViewProps> = ({ children, ...props }) => {
-    return <svg {...props}>{children}</svg>;
-  };
-}
+export type GroupProps = HTMLAttributes<SVGGElement> & {};
+
+export const Group: FC<GroupProps> = ({ children, ...props }) => {
+  return <g {...props}>{children}</g>;
+};
+
+export type ViewProps = HTMLAttributes<SVGElement> & {};
+
+export const View: FC<ViewProps> = ({ children, ...props }) => {
+  return <svg {...props}>{children}</svg>;
+};
