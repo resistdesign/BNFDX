06d807ea5284885be81215776bcf9773a13229a1
Ryan Graff <ryantgraff@resistdesign.com>
2021-02-17 01:44:00 -0500

    fix: (parsePathD) Fix compressed style, non-delimited coordinate parsing.

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ export type PathCommand = {
 
 export const parsePathD = (d = ''): PathCommand[] =>
   d
+    .replace(/\n/gm, ' ')
     .replace(/[a-z]/gim, (char) => ` :${char} `)
-    .replace(/[+-]/gim, (char) => ` ${char}`)
-    .replace(/,/gim, ' ')
+    .replace(/[+-]/gm, (char) => ` ${char}`)
+    .replace(/,/gm, ' ')
     .split(' ')
     .filter((chars) => chars !== '')
     .join(' ')
@@ -21,6 +22,39 @@ export const parsePathD = (d = ''): PathCommand[] =>
       const coords = data
         .split(' ')
         .filter((chars, i) => !!chars && i > 0)
+        .reduce((acc, chars) => {
+          const charList = chars.split('');
+          const values: string[] = [];
+
+          let currentValue = '',
+            hadDec = false;
+
+          for (const c of charList) {
+            if (c === '+' || c === '-') {
+              if (currentValue !== '') {
+                values.push(currentValue);
+              }
+
+              currentValue = c;
+              hadDec = false;
+            } else if (c === '.' && hadDec) {
+              values.push(currentValue);
+
+              currentValue = c;
+            } else if (c === '.') {
+              currentValue += c;
+              hadDec = true;
+            } else {
+              currentValue += c;
+            }
+          }
+
+          if (currentValue !== '') {
+            values.push(currentValue);
+          }
+
+          return [...acc, ...values];
+        }, [] as string[])
         .map((v) => parseFloat(v));
       const { length } = coords;
 
@@ -33,6 +67,9 @@ export const parsePathD = (d = ''): PathCommand[] =>
         },
       ];
     }, [] as PathCommand[]);
+
+export const pathCommandsToPathD = (pathCommands: PathCommand[] = []): string =>
+  pathCommands.reduce((acc, { command, coords }) => [...acc, command, ...coords], [] as (string | number)[]).join(' ');
 
 export type PathProps = HTMLAttributes<SVGPathElement> & {};
 
--- stories/R3DM.stories.tsx
+++ stories/R3DM.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { parsePathD, View, ViewProps } from '../src';
+import { parsePathD, pathCommandsToPathD, View, ViewProps } from '../src';
 
 const PATH_D =
   'm25.645 38.743c2.061 2.882 8.35 3.09 10.51 1.868.793-.449.991-1.747.991-2.627 0-.604-.159-1.079-.477-1.424-.336-.345-1.189-1.221-2.178-1.48-5.02-1.294-8.492-2.911-9.852-4.085-1.696-1.467-1.969-2.425-1.969-4.979 0-2.554.734-4.695 2.395-6.231 1.66-1.536 4.071-2.304 7.233-2.304 3.02 0 7.268.797 9.339 3.256l-3.453 3.756c-1.947-1.635-3.396-1.783-5.78-1.783-1.501 0-2.283.216-2.919.647-.636.414-.954.958-.954 1.631 0 .604.291 1.105.874 1.501.583.414 2.106 1.036 5.144 1.864 3.409.932 5.723 2.062 6.942 3.391 1.201 1.329 1.802 3.098 1.802 5.307 0 2.658-.936 4.85-2.808 6.575-1.855 1.726-4.138 3.067-7.423 3.067-4.229 0-9.874-1.494-11.431-4.255';
+const PATH_COMMANDS = parsePathD(PATH_D);
+const REBUILT_PATH_D = pathCommandsToPathD(PATH_COMMANDS);
 
 const meta: Meta = {
   title: 'View',
@@ -26,7 +28,10 @@ const Template: Story<ViewProps> = (args) => (
   <>
     Path Data:
     <br />
-    <pre>{JSON.stringify(parsePathD(PATH_D), null, 2)}</pre>
+    <pre>{JSON.stringify(PATH_COMMANDS, null, 2)}</pre>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+      <path d={REBUILT_PATH_D} />
+    </svg>
     <View {...args} />
   </>
 );
