93f505b39cd2c2ff7a19caac833ed445b96eeab5
Ryan Graff <ryantgraff@resistdesign.com>
2021-02-28 10:50:05 -0500

    feat: (svg-parser) Implement login for each processor in TOKEN_VALIDATOR_OPTION_PROCESSORS. Improve all processor related signatures and types.

--- svg-parser/syntax-string-parser.ts
+++ svg-parser/syntax-string-parser.ts
@@ -1,7 +1,7 @@
 export enum TokenProcessorOptionTypes {
-  '*' = '*',
-  '+' = '+',
-  '?' = '?',
+  ZERO_OR_MORE = '*',
+  ONE_OR_MORE = '+',
+  ZERO_OR_ONE = '?',
 }
 
 export type TokenValidatorOptionsConfig<TokenTypes extends string> = {
@@ -24,77 +24,187 @@ export type Grammar<TokenTypes extends string> = {
   map: BaseGrammarMapType<TokenTypes>;
 };
 
-export type AST<TokenTypes extends string> =
-  | {
-      startIndex: number;
-      endIndex: number;
-      tokenType: TokenTypes;
-      value: string | AST<TokenTypes>;
-    }
-  | AST<TokenTypes>[];
-
-type TokenValidatorOptionProcessorResults =
-  | {
-      consumedString: string;
-      newCurrentIndex: number;
-    }
-  | false;
-type TokenValidatorOptionProcessor = <TokenTypes extends string>(
-  syntaxString: string,
-  currentIndex: number,
-  tokenValidator: TokenValidator<TokenTypes>
-) => TokenValidatorOptionProcessorResults;
-type TokenValidatorOptionProcessorMap = {
-  [key in TokenProcessorOptionTypes]: TokenValidatorOptionProcessor;
+export type AST<TokenTypes extends string> = {
+  startIndex: number;
+  endIndex: number;
+  tokenType: TokenTypes;
+  value: string | AST<TokenTypes> | AST<TokenTypes>[];
 };
 
-const PROCESS_REGEX = (syntaxString: string, currentIndex: number, tokenValidator: RegExp): TokenValidatorOptionProcessorResults => {
-  const stringFromIndex = syntaxString.slice(currentIndex);
-  const match = stringFromIndex.match(tokenValidator);
-
-  if (match) {
-    const consumedString = match[0] || '';
-
-    return {
-      consumedString,
-      newCurrentIndex: currentIndex + consumedString.length,
-    };
-  }
-
-  return false;
-};
-// TODO: These need to return (and probably receive) the same things as `processTokenValidator`,
-//  as well as option related info.
-const TOKEN_VALIDATOR_OPTION_PROCESSORS: TokenValidatorOptionProcessorMap = {
-  [TokenProcessorOptionTypes['*']]: <TokenTypes extends string>(
-    syntaxString: string,
-    currentIndex: number,
-    tokenValidator: TokenValidator<TokenTypes>
-  ): TokenValidatorOptionProcessorResults => {},
-  [TokenProcessorOptionTypes['+']]: <TokenTypes extends string>(
-    syntaxString: string,
-    currentIndex: number,
-    tokenValidator: TokenValidator<TokenTypes>
-  ): TokenValidatorOptionProcessorResults => {},
-  [TokenProcessorOptionTypes['?']]: <TokenTypes extends string>(
-    syntaxString: string,
-    currentIndex: number,
-    tokenValidator: TokenValidator<TokenTypes>
-  ): TokenValidatorOptionProcessorResults => {},
-};
-
-const processTokenValidator = <TokenTypes extends string>(
+type TokenValidationProcessorReturnValue<TokenTypes extends string> = AST<TokenTypes> | false;
+type TokenValidationProcessor = <TokenTypes extends string>(
   syntaxString: string,
   tokenType: TokenTypes,
   tokenValidator: TokenValidator<TokenTypes>,
   grammarMap: BaseGrammarMapType<TokenTypes>,
-  currentIndex
+  currentIndex: number
+) => TokenValidationProcessorReturnValue<TokenTypes>;
+type TokenValidatorOptionProcessorMap = {
+  [key in TokenProcessorOptionTypes]: TokenValidationProcessor;
+};
+
+const PROCESS_REGEX: TokenValidationProcessor = <TokenTypes extends string>(
+  syntaxString: string,
+  tokenType: TokenTypes,
+  tokenValidator: TokenValidator<TokenTypes>,
+  _grammarMap: BaseGrammarMapType<TokenTypes>,
+  currentIndex: number
+): TokenValidationProcessorReturnValue<TokenTypes> => {
+  if (tokenValidator instanceof RegExp) {
+    const stringFromIndex = syntaxString.slice(currentIndex);
+    const match = stringFromIndex.match(tokenValidator);
+
+    if (match) {
+      const consumedString = match[0] || '';
+
+      return {
+        startIndex: currentIndex,
+        endIndex: currentIndex + (consumedString.length - 1),
+        value: consumedString,
+        tokenType,
+      };
+    }
+  }
+
+  return false;
+};
+const TOKEN_VALIDATOR_OPTION_PROCESSORS: TokenValidatorOptionProcessorMap = {
+  [TokenProcessorOptionTypes.ZERO_OR_MORE]: <TokenTypes extends string>(
+    syntaxString: string,
+    tokenType: TokenTypes,
+    tokenValidator: TokenValidator<TokenTypes>,
+    grammarMap: BaseGrammarMapType<TokenTypes>,
+    currentIndex: number
+  ): TokenValidationProcessorReturnValue<TokenTypes> => {
+    const results: AST<TokenTypes>[] = [];
+
+    let latestCurrentIndex = currentIndex;
+
+    while (true) {
+      const resultAst = processTokenValidator(syntaxString, tokenType, tokenValidator, grammarMap, latestCurrentIndex);
+
+      if (resultAst) {
+        const { endIndex } = resultAst;
+
+        latestCurrentIndex = endIndex + 1;
+
+        results.push(resultAst);
+      } else {
+        break;
+      }
+    }
+
+    return results.length > 0
+      ? {
+          startIndex: currentIndex,
+          endIndex: latestCurrentIndex - 1,
+          value: results,
+          tokenType: tokenType,
+        }
+      : // IMPORTANT: This processor is ALWAYS VALID, but it can return an "empty" result.
+        {
+          startIndex: currentIndex,
+          endIndex: currentIndex,
+          value: '',
+          tokenType,
+        };
+  },
+  [TokenProcessorOptionTypes.ONE_OR_MORE]: <TokenTypes extends string>(
+    syntaxString: string,
+    tokenType: TokenTypes,
+    tokenValidator: TokenValidator<TokenTypes>,
+    grammarMap: BaseGrammarMapType<TokenTypes>,
+    currentIndex: number
+  ): TokenValidationProcessorReturnValue<TokenTypes> => {
+    const results: AST<TokenTypes>[] = [];
+
+    let latestCurrentIndex = currentIndex;
+
+    while (true) {
+      const resultAst = processTokenValidator(syntaxString, tokenType, tokenValidator, grammarMap, latestCurrentIndex);
+
+      if (resultAst) {
+        const { endIndex } = resultAst;
+
+        latestCurrentIndex = endIndex + 1;
+
+        results.push(resultAst);
+      } else {
+        break;
+      }
+    }
+
+    return results.length > 0
+      ? {
+          startIndex: currentIndex,
+          endIndex: latestCurrentIndex - 1,
+          value: results,
+          tokenType: tokenType,
+        }
+      : // IMPORTANT: This processor is ONLY VALID if there is AT LEAST ONE result.
+        false;
+  },
+  [TokenProcessorOptionTypes.ZERO_OR_ONE]: <TokenTypes extends string>(
+    syntaxString: string,
+    tokenType: TokenTypes,
+    tokenValidator: TokenValidator<TokenTypes>,
+    grammarMap: BaseGrammarMapType<TokenTypes>,
+    currentIndex: number
+  ): TokenValidationProcessorReturnValue<TokenTypes> => {
+    let singleResult: AST<TokenTypes> | undefined,
+      latestCurrentIndex: number = currentIndex,
+      tooManyResults: boolean = false;
+
+    for (let i = 0; i < 2; i++) {
+      const resultAst = processTokenValidator(syntaxString, tokenType, tokenValidator, grammarMap, latestCurrentIndex);
+
+      if (resultAst) {
+        // IMPORTANT: Check for a second result, if it is found, then this processor is invalid.
+        if (i === 1) {
+          tooManyResults = true;
+
+          break;
+        } else {
+          const { endIndex } = resultAst;
+
+          latestCurrentIndex = endIndex + 1;
+
+          singleResult = resultAst;
+        }
+      } else {
+        break;
+      }
+    }
+
+    return !tooManyResults
+      ? !singleResult
+        ? // ZERO RESULTS
+          {
+            startIndex: currentIndex,
+            endIndex: currentIndex,
+            value: '',
+            tokenType,
+          }
+        : // ONE RESULT
+          singleResult
+      : // IMPORTANT: This processor is ONLY VALID if there is ONLY ZERO results OR ONLY ONE result.
+        false;
+  },
+};
+
+const processTokenValidator: TokenValidationProcessor = <TokenTypes extends string>(
+  syntaxString: string,
+  tokenType: TokenTypes,
+  tokenValidator: TokenValidator<TokenTypes>,
+  grammarMap: BaseGrammarMapType<TokenTypes>,
+  currentIndex: number
 ): AST<TokenTypes> | false => {
   if (tokenValidator instanceof Array) {
     // A combination of types.
     const resultList = [];
 
     for (const t of tokenValidator) {
+      // TODO: Increment the `currentIndex`.
       const result = processTokenValidator<TokenTypes>(syntaxString, tokenType, t, grammarMap, currentIndex);
 
       if (!result) {
